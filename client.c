d87f87b44f5c7bfa51051bda456b2c134e7567c7
Angelus Vincent <angelus.vincent@campus.tu-berlin.de>
2024-06-30 19:31:45 +0200

    added the address from rasberry pi

--- client.c
+++ client.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define PORT 8000
+#define PORT 8080
 
 int main() {
   int sock = 0;
@@ -21,7 +21,7 @@ int main() {
   serv_addr.sin_port = htons(PORT);
 
   // Convert IPv4 and IPv6 addresses from text to binary form
-  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+  if (inet_pton(AF_INET, "192.168.1.219", &serv_addr.sin_addr) <= 0) {
     printf("\nInvalid address/ Address not supported\n");
     return -1;
   }
